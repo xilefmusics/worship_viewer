afdbfe1dde2202879cd4404322b59c54516b7afc
Xilefmusics <xilefmusics@gmail.com>
2021-04-16 12:56:55 +0200

    tested from_str

--- src/line/wp/from_str.rs
+++ src/line/wp/from_str.rs
@@ -43,3 +43,88 @@ pub trait IntoFromStr<'a>: Iterator {
 }
 
 impl<'a, I> IntoFromStr<'a> for I where I: Sized + Iterator<Item = &'a str> {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        let vec = "".lines().to_wp().collect::<Vec<Line>>();
+        assert_eq!(vec.len(), 0);
+    }
+
+    #[test]
+    fn directive() {
+        let vec = "{key:value}".lines().to_wp().collect::<Vec<Line>>();
+        assert_eq!(
+            vec,
+            vec!(Line::Directive(("key".to_string(), "value".to_string())))
+        );
+    }
+
+    #[test]
+    fn directive_spaces() {
+        let vec = "{ key  :   value    }"
+            .lines()
+            .to_wp()
+            .collect::<Vec<Line>>();
+        assert_eq!(
+            vec,
+            vec!(Line::Directive(("key".to_string(), "value".to_string())))
+        );
+    }
+
+    #[test]
+    fn directive_unicode() {
+        let vec = "{äber:v'al}".lines().to_wp().collect::<Vec<Line>>();
+        assert_eq!(
+            vec,
+            vec!(Line::Directive(("äber".to_string(), "v'al".to_string())))
+        );
+    }
+
+    #[test]
+    fn asci() {
+        let vec = "This is a [C]line & Das ist eine Zeile"
+            .lines()
+            .to_wp()
+            .collect::<Vec<Line>>();
+        assert_eq!(
+            vec,
+            vec!(Line::TextChordTrans(
+                "This is a [C]line & Das ist eine Zeile".to_string(),
+            ))
+        );
+    }
+
+    #[test]
+    fn asci_spaces() {
+        let vec = " Hello ".lines().to_wp().collect::<Vec<Line>>();
+        assert_eq!(vec, vec!(Line::TextChordTrans("Hello".to_string(),)));
+    }
+
+    #[test]
+    fn unicode() {
+        let vec = "Hällo['ö]".lines().to_wp().collect::<Vec<Line>>();
+        assert_eq!(vec, vec!(Line::TextChordTrans("Hällo['ö]".to_string(),)));
+    }
+
+    #[test]
+    fn multiline() {
+        let vec = "{title: The Title}
+{key: The Key}
+This is a line"
+            .lines()
+            .to_wp()
+            .collect::<Vec<Line>>();
+        assert_eq!(
+            vec,
+            vec!(
+                Line::Directive(("title".to_string(), "The Title".to_string())),
+                Line::Directive(("key".to_string(), "The Key".to_string())),
+                Line::TextChordTrans("This is a line".to_string())
+            )
+        );
+    }
+}
