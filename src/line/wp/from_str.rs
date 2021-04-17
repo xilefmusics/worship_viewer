e583340249f9ea0860ae61b5d3e7c6be02e57c9b
Xilefmusics <xilefmusics@gmail.com>
2021-04-17 13:32:38 +0200

    interpret not correct directives as text

--- src/line/wp/from_str.rs
+++ src/line/wp/from_str.rs
@@ -2,17 +2,22 @@ use super::{Directive, Empty, Line, TextChordTrans};
 
 pub fn from_str(string: &str) -> Line {
     let string = string.trim();
-    match string.chars().next() {
-        Some('{') => {
-            if let Some(colon_idx) = string.find(":") {
-                let key = String::from((&string[1..colon_idx]).trim());
-                let value = String::from((&string[colon_idx + 1..string.len() - 1]).trim());
-                return Directive((key, value));
+    if let Some(start_idx) = string.find("{") {
+        if let Some(colon_idx) = string.find(":") {
+            if let Some(end_idx) = string.find("}") {
+                if start_idx < colon_idx && colon_idx < end_idx {
+                    if start_idx == 0 && end_idx == string.len() - 1 {
+                        let key = String::from((&string[start_idx + 1..colon_idx]).trim());
+                        let value = String::from((&string[colon_idx + 1..end_idx]).trim());
+                        if key.len() > 0 && value.len() > 0 {
+                            return Directive((key, value));
+                        }
+                    }
+                }
             }
-            Empty
         }
-        _ => TextChordTrans(string.to_string()),
     }
+    TextChordTrans(string.to_string())
 }
 
 pub struct FromStr<'a, I>
@@ -125,6 +130,62 @@ This is a line"
                 Line::Directive(("key".to_string(), "The Key".to_string())),
                 Line::TextChordTrans("This is a line".to_string())
             )
+        );
+    }
+
+    #[test]
+    fn non_closing_directive() {
+        let vec = "{key:value".lines().to_wp().collect::<Vec<Line>>();
+        assert_eq!(vec, vec!(Line::TextChordTrans("{key:value".to_string())));
+    }
+
+    #[test]
+    fn non_seperating_directive() {
+        let vec = "{word}".lines().to_wp().collect::<Vec<Line>>();
+        assert_eq!(vec, vec!(Line::TextChordTrans("{word}".to_string())));
+    }
+
+    #[test]
+    fn non_key_directive() {
+        let vec = "{:value}".lines().to_wp().collect::<Vec<Line>>();
+        assert_eq!(vec, vec!(Line::TextChordTrans("{:value}".to_string())));
+    }
+
+    #[test]
+    fn non_value_directive() {
+        let vec = "{key:}".lines().to_wp().collect::<Vec<Line>>();
+        assert_eq!(vec, vec!(Line::TextChordTrans("{key:}".to_string())));
+    }
+
+    #[test]
+    fn empty_directive() {
+        let vec = "{}".lines().to_wp().collect::<Vec<Line>>();
+        assert_eq!(vec, vec!(Line::TextChordTrans("{}".to_string())));
+    }
+
+    #[test]
+    fn bullshit_directive() {
+        let vec = ":}{".lines().to_wp().collect::<Vec<Line>>();
+        assert_eq!(vec, vec!(Line::TextChordTrans(":}{".to_string())));
+    }
+
+    #[test]
+    fn directive_followed() {
+        let vec = "{key:value}following"
+            .lines()
+            .to_wp()
+            .collect::<Vec<Line>>();
+        assert_eq!(
+            vec,
+            vec!(Line::TextChordTrans("{key:value}following".to_string()))
+        );
+    }
+    #[test]
+    fn directive_following() {
+        let vec = "pre{key:value}".lines().to_wp().collect::<Vec<Line>>();
+        assert_eq!(
+            vec,
+            vec!(Line::TextChordTrans("pre{key:value}".to_string()))
         );
     }
 }
