ce8a318a4c483eddfc16fca935e7289d373b17f8
Xilefmusics <xilefmusics@gmail.com>
2021-10-05 10:58:28 +0200

    more umlauts

--- src/tui/replace_umlaut.rs
+++ src/tui/replace_umlaut.rs
@@ -1,3 +1,11 @@
 pub fn replace_umlaut(string: String) -> String {
-    string.replace("ä", "a").replace("ö", "o").replace("ü", "u").replace("ß", "s")
+    string
+        .replace("ä", "a")
+        .replace("Ä", "A")
+        .replace("ö", "o")
+        .replace("Ö", "O")
+        .replace("ü", "u")
+        .replace("Ü", "U")
+        .replace("ß", "s")
+        .replace("’", "'")
 }
