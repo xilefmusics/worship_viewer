f0e060a9073c0c20b7e306b2dc0c70ab15a54213
Xilefmusics <xilefmusics@gmail.com>
2021-10-21 15:14:28 +0200

    version 0.2.4

--- static/service-worker.js
+++ static/service-worker.js
@@ -6,7 +6,8 @@ self.addEventListener('install', event => event.waitUntil(
         '/',
         '/static/global.css',
         '/static/bundle.css',
-        '/static/bundle.js.map',
+        '/static/bundle.js',
+        '/build/bundle.js.map',
         '/static/manifest.json',
         '/static/favicon.png',
         '/static/service-worker.js',
@@ -14,41 +15,33 @@ self.addEventListener('install', event => event.waitUntil(
 ));
 
 // handle fetch events
-self.addEventListener('fetch', async event => {
+self.addEventListener('fetch', event => {
+    // return if request isn't get
     if (event.request.method !== 'GET') {
         return;
     }
-
-    console.log('try getting from remote');
-    try {
-        const remote = await fetch(event.request);
-        caches.open(version + 'fundamentals').then(cache => cache.put(event.request, remote));
-        console.log(remote);
-        return remote;
-    } catch {}
-
-    console.log('try getting from cache');
-    const local = await caches.match(event.request);
-    if (local) {
-        console.log(local);
-        return local;
-    }
-
-    console.log(event.request);
-    console.log('not in remote and not in cache');
-    return new Response('<h1>Service Unavailable</h1>', {
-        status: 503,
-        statusText: 'Service Unavailable',
-        headers: new Headers({
-          'Content-Type': 'text/html'
-        })
-      });
-});
-
-// remove older versions of service worker
-self.addEventListener("activate", async event => {
-    console.log("ONACTIVATE: start");
-    const keys = cache.keys();
-    Promise.all(keys.filter(key => !key.startsWith(version)).map(key => caches.delete(key)));
-    console.log("ONACTIVATE: finished");
+    // respond to request
+    event.respondWith(caches.match(event.request).then( async local => {
+        // try to send back remote response
+        try {
+            const remote = await fetch(event.request.clone());
+            // update local cache if response was already cached
+            if (local) {
+                caches.open(version + 'fundamentals').then(cache => cache.put(event.request, remote));
+            }
+            return remote.clone();
+        } catch {}
+        // try to send back local response
+        if (local) {
+            return local;
+        }
+        // send back default response
+        return new Response('<h1>Service Unavailable</h1>', {
+            status: 503,
+            statusText: 'Service Unavailable',
+            headers: new Headers({
+                'Content-Type': 'text/html'
+            }),
+        });
+    }));
 });
