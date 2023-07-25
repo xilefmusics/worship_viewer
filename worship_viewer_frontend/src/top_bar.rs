071eacd4df16b84ebd10e98964785b8ea7b69e54
Felix Rollbühler <xilef@Air-von-Felix.fritz.box>
2023-07-25 14:23:29 +0200

    Add search icon to search bar

--- worship_viewer_frontend/src/top_bar.rs
+++ worship_viewer_frontend/src/top_bar.rs
@@ -1,7 +1,5 @@
-use crate::routes::Route;
 use stylist::Style;
 use yew::prelude::*;
-use yew_router::prelude::*;
 
 #[derive(Properties, PartialEq, Clone)]
 pub struct Props {
@@ -11,13 +9,13 @@ pub struct Props {
 
 #[function_component]
 pub fn TopBarComponent(props: &Props) -> Html {
-    let navigator = use_navigator().unwrap();
     let search_placeholder = props.search_placeholder.clone();
 
     html! {
         <div class={Style::new(include_str!("top_bar.css")).expect("Unwrapping CSS should work!")}>
             <div class="left"></div>
             <div class="center">
+                <span class="material-symbols-outlined search-icon">{"search"}</span>
                 <input type="text" placeholder={search_placeholder} />
             </div>
             <div class={"right"}>
