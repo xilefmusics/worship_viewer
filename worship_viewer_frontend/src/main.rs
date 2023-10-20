770340a571fe1e8aa6636285b77343775a6d8898
Felix Rollbühler <xilef@MacBook-Air-von-Felix.local>
2023-10-20 21:42:25 +0200

    Set overscroll behaviour to none to prevent reload ob mobile devices

--- worship_viewer_frontend/src/main.rs
+++ worship_viewer_frontend/src/main.rs
@@ -32,7 +32,7 @@ fn switch(routes: Route) -> Html {
 fn App() -> Html {
     html! {
         <>
-            <Global css={css!("html,body{padding: 0;margin: 0;border: 0;background: #1e1e1e; overflow: hidden;}")} />
+            <Global css={css!("html,body{padding: 0;margin: 0;border: 0;background: #1e1e1e; overflow: hidden; overscroll-behavior: none; }")} />
             <div class={Style::new(include_str!("style.css")).expect("Unwrapping CSS should work!")}>
                 <div class={"app"}>
                     <BrowserRouter>
