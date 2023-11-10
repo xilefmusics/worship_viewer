964c85fda841f013c5cf1da15aa8d3f255b44794
Felix Rollbühler <xilef@MacBook-Air-von-Felix.local>
2023-11-10 15:16:44 +0100

    add collections endpoint

--- worship_viewer_backend/src/types/mod.rs
+++ worship_viewer_backend/src/types/mod.rs
@@ -4,11 +4,13 @@ use surrealdb::sql::Id;
 use crate::AppError;
 
 mod blob;
+mod collection;
 mod group;
 mod song;
 mod user;
 
 pub use blob::{Blob, BlobDatabase};
+pub use collection::{Collection, CollectionDatabase};
 pub use group::{Group, GroupDatabase};
 pub use song::{Song, SongDatabase};
 pub use user::{User, UserDatabase};
