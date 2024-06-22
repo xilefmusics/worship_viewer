fe8cd170b4f3bc2320c0cb2e646ff14d343240ed
Felix Rollbühler <xilef@xilefbook.fritz.box>
2024-06-22 16:10:06 +0200

    svg support

--- backend/src/types/blob.rs
+++ backend/src/types/blob.rs
@@ -12,6 +12,8 @@ pub enum FileType {
     PNG,
     #[serde(rename(deserialize = "image/jpeg", serialize = "image/jpeg"))]
     JPEG,
+    #[serde(rename(deserialize = "image/svg", serialize = "image/svg"))]
+    SVG,
 }
 
 impl FileType {
@@ -19,6 +21,7 @@ impl FileType {
         match self {
             Self::PNG => ".png",
             Self::JPEG => ".jpeg",
+            Self::SVG => ".svg",
         }
     }
 }
