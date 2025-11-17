98153f4e14a65c59dba9cc73f53520ce4f141ae2
Felix Rollbühler <xilef@Mac.fritz.box>
2025-11-17 16:06:04 +0100

    implement frontend API

--- frontend/src/api/api.rs
+++ frontend/src/api/api.rs
@@ -2,7 +2,18 @@ use gloo_net::http::Request;
 use serde::{de::DeserializeOwned, Serialize};
 use yew_router::prelude::Navigator;
 
-use super::{ApiError, ErrorResponse, OtpRequestPayload, OtpVerifyPayload, Session, User};
+use shared::blob::Blob;
+use shared::blob::CreateBlob;
+use shared::collection::Collection;
+use shared::collection::CreateCollection;
+use shared::like::LikeStatus;
+use shared::player::Player;
+use shared::setlist::CreateSetlist;
+use shared::setlist::Setlist;
+use shared::song::CreateSong;
+use shared::song::Song;
+
+use super::{ApiError, CreateUserRequest, ErrorResponse, OtpRequestPayload, OtpVerifyPayload, Session, User};
 use crate::route::Route;
 
 #[derive(Clone, PartialEq)]
@@ -97,8 +108,154 @@ impl Api {
         }
     }
 
-    pub async fn get_users_me(&self) -> Result<User, ApiError> {
-        self.get("/api/v1/users/me").await
+    async fn put<T, B>(&self, path: &str, body: &B) -> Result<T, ApiError>
+    where
+        T: DeserializeOwned + Default,
+        B: ?Sized + Serialize,
+    {
+        let response = Request::put(&Self::build_path(path))
+            .credentials(web_sys::RequestCredentials::Include)
+            .json(body)?
+            .send()
+            .await?;
+
+        match response.status() {
+            200 | 201 => Ok(response.json::<T>().await?),
+            204 => Ok(T::default()),
+            status => Err(
+                match response.json::<ErrorResponse>().await?.to_api_error(status) {
+                    ApiError::Unauthorized(msg) => {
+                        self.route_logout();
+                        ApiError::Unauthorized(msg)
+                    }
+                    err => err,
+                },
+            ),
+        }
+    }
+
+    async fn delete<T>(&self, path: &str) -> Result<T, ApiError>
+    where
+        T: DeserializeOwned + Default,
+    {
+        let response = Request::delete(&Self::build_path(path))
+            .credentials(web_sys::RequestCredentials::Include)
+            .send()
+            .await?;
+
+        match response.status() {
+            200 | 201 => Ok(response.json::<T>().await?),
+            204 => Ok(T::default()),
+            status => Err(
+                match response.json::<ErrorResponse>().await?.to_api_error(status) {
+                    ApiError::Unauthorized(msg) => {
+                        self.route_logout();
+                        ApiError::Unauthorized(msg)
+                    }
+                    err => err,
+                },
+            ),
+        }
+    }
+
+    async fn get_entity<T>(&self, path: &str) -> Result<T, ApiError>
+    where
+        T: DeserializeOwned,
+    {
+        let response = Request::get(&Self::build_path(path))
+            .credentials(web_sys::RequestCredentials::Include)
+            .send()
+            .await?;
+
+        match response.status() {
+            200 | 201 => Ok(response.json::<T>().await?),
+            204 => Err(ApiError::InternalServerError("Unexpected 204 response for entity retrieval".into())),
+            status => Err(
+                match response.json::<ErrorResponse>().await?.to_api_error(status) {
+                    ApiError::Unauthorized(msg) => {
+                        self.route_logout();
+                        ApiError::Unauthorized(msg)
+                    }
+                    err => err,
+                },
+            ),
+        }
+    }
+
+    async fn put_entity<T, B>(&self, path: &str, body: &B) -> Result<T, ApiError>
+    where
+        T: DeserializeOwned,
+        B: ?Sized + Serialize,
+    {
+        let response = Request::put(&Self::build_path(path))
+            .credentials(web_sys::RequestCredentials::Include)
+            .json(body)?
+            .send()
+            .await?;
+
+        match response.status() {
+            200 | 201 => Ok(response.json::<T>().await?),
+            204 => Err(ApiError::InternalServerError("Unexpected 204 response for entity update".into())),
+            status => Err(
+                match response.json::<ErrorResponse>().await?.to_api_error(status) {
+                    ApiError::Unauthorized(msg) => {
+                        self.route_logout();
+                        ApiError::Unauthorized(msg)
+                    }
+                    err => err,
+                },
+            ),
+        }
+    }
+
+    async fn post_entity<T, B>(&self, path: &str, body: &B) -> Result<T, ApiError>
+    where
+        T: DeserializeOwned,
+        B: ?Sized + Serialize,
+    {
+        let response = Request::post(&Self::build_path(path))
+            .credentials(web_sys::RequestCredentials::Include)
+            .json(body)?
+            .send()
+            .await?;
+
+        match response.status() {
+            200 | 201 => Ok(response.json::<T>().await?),
+            204 => Err(ApiError::InternalServerError("Unexpected 204 response for entity creation".into())),
+            status => Err(
+                match response.json::<ErrorResponse>().await?.to_api_error(status) {
+                    ApiError::Unauthorized(msg) => {
+                        self.route_logout();
+                        ApiError::Unauthorized(msg)
+                    }
+                    err => err,
+                },
+            ),
+        }
+    }
+
+    async fn delete_entity<T>(&self, path: &str) -> Result<T, ApiError>
+    where
+        T: DeserializeOwned,
+    {
+        let response = Request::delete(&Self::build_path(path))
+            .credentials(web_sys::RequestCredentials::Include)
+            .send()
+            .await?;
+
+        match response.status() {
+            200 | 201 => Ok(response.json::<T>().await?),
+            204 => Err(ApiError::InternalServerError("Unexpected 204 response for entity deletion".into())),
+            status => Err(
+                match response.json::<ErrorResponse>().await?.to_api_error(status) {
+                    ApiError::Unauthorized(msg) => {
+                        self.route_logout();
+                        ApiError::Unauthorized(msg)
+                    }
+                    err => err,
+                },
+            ),
+        }
     }
 
     pub async fn request_otp(&self, email: String) -> Result<(), ApiError> {
@@ -134,5 +291,184 @@ impl Api {
 
     pub fn route_index(&self) {
         self.navigator.push(&Route::Index);
+    }
+
+    pub async fn get_users(&self) -> Result<Vec<User>, ApiError> {
+        self.get("/api/v1/users").await
+    }
+
+    pub async fn get_user(&self, id: &str) -> Result<User, ApiError> {
+        self.get(&format!("/api/v1/users/{}", id)).await
+    }
+
+    pub async fn create_user(&self, payload: &CreateUserRequest) -> Result<User, ApiError> {
+        self.post("/api/v1/users", payload).await
+    }
+
+    pub async fn delete_user(&self, id: &str) -> Result<User, ApiError> {
+        self.delete(&format!("/api/v1/users/{}", id)).await
+    }
+
+    pub async fn get_users_me(&self) -> Result<User, ApiError> {
+        self.get("/api/v1/users/me").await
+    }
+
+    pub async fn get_sessions_for_current_user(&self) -> Result<Vec<Session>, ApiError> {
+        self.get("/api/v1/users/me/sessions").await
+    }
+
+    pub async fn get_session_for_current_user(&self, id: &str) -> Result<Session, ApiError> {
+        self.get(&format!("/api/v1/users/me/sessions/{}", id)).await
+    }
+
+    pub async fn delete_session_for_current_user(&self, id: &str) -> Result<Session, ApiError> {
+        self.delete(&format!("/api/v1/users/me/sessions/{}", id)).await
+    }
+
+    pub async fn get_sessions_for_user(&self, user_id: &str) -> Result<Vec<Session>, ApiError> {
+        self.get(&format!("/api/v1/users/{}/sessions", user_id)).await
+    }
+
+    pub async fn get_session_for_user(&self, user_id: &str, id: &str) -> Result<Session, ApiError> {
+        self.get(&format!("/api/v1/users/{}/sessions/{}", user_id, id)).await
+    }
+
+    pub async fn create_session_for_user(&self, user_id: &str) -> Result<Session, ApiError> {
+        self.post_empty(&format!("/api/v1/users/{}/sessions", user_id)).await
+    }
+
+    pub async fn delete_session_for_user(&self, user_id: &str, id: &str) -> Result<Session, ApiError> {
+        self.delete(&format!("/api/v1/users/{}/sessions/{}", user_id, id)).await
+    }
+
+    pub async fn get_songs(&self) -> Result<Vec<Song>, ApiError> {
+        self.get("/api/v1/songs").await
+    }
+
+    pub async fn get_song(&self, id: &str) -> Result<Song, ApiError> {
+        self.get(&format!("/api/v1/songs/{}", id)).await
+    }
+
+    pub async fn get_song_player(&self, id: &str) -> Result<Player, ApiError> {
+        self.get(&format!("/api/v1/songs/{}/player", id)).await
+    }
+
+    pub async fn create_song(&self, payload: &CreateSong) -> Result<Song, ApiError> {
+        self.post_entity("/api/v1/songs", payload).await
+    }
+
+    pub async fn update_song(&self, id: &str, payload: &CreateSong) -> Result<Song, ApiError> {
+        self.put_entity(&format!("/api/v1/songs/{}", id), payload).await
+    }
+
+    pub async fn delete_song(&self, id: &str) -> Result<Song, ApiError> {
+        self.delete_entity(&format!("/api/v1/songs/{}", id)).await
+    }
+
+    pub async fn get_song_like_status(&self, id: &str) -> Result<LikeStatus, ApiError> {
+        self.get(&format!("/api/v1/songs/{}/likes", id)).await
+    }
+
+    pub async fn update_song_like_status(&self, id: &str, payload: &LikeStatus) -> Result<LikeStatus, ApiError> {
+        self.put(&format!("/api/v1/songs/{}/likes", id), payload).await
+    }
+
+    pub async fn get_collections(&self) -> Result<Vec<Collection>, ApiError> {
+        self.get("/api/v1/collections").await
+    }
+
+    pub async fn get_collection(&self, id: &str) -> Result<Collection, ApiError> {
+        self.get(&format!("/api/v1/collections/{}", id)).await
+    }
+
+    pub async fn get_collection_songs(&self, id: &str) -> Result<Vec<Song>, ApiError> {
+        self.get(&format!("/api/v1/collections/{}/songs", id)).await
+    }
+
+    pub async fn get_collection_player(&self, id: &str) -> Result<Player, ApiError> {
+        self.get(&format!("/api/v1/collections/{}/player", id)).await
+    }
+
+    pub async fn create_collection(&self, payload: &CreateCollection) -> Result<Collection, ApiError> {
+        self.post_entity("/api/v1/collections", payload).await
+    }
+
+    pub async fn update_collection(&self, id: &str, payload: &CreateCollection) -> Result<Collection, ApiError> {
+        self.put_entity(&format!("/api/v1/collections/{}", id), payload).await
+    }
+
+    pub async fn delete_collection(&self, id: &str) -> Result<Collection, ApiError> {
+        self.delete_entity(&format!("/api/v1/collections/{}", id)).await
+    }
+
+    pub async fn get_setlists(&self) -> Result<Vec<Setlist>, ApiError> {
+        self.get("/api/v1/setlists").await
+    }
+
+    pub async fn get_setlist(&self, id: &str) -> Result<Setlist, ApiError> {
+        self.get(&format!("/api/v1/setlists/{}", id)).await
+    }
+
+    pub async fn get_setlist_songs(&self, id: &str) -> Result<Vec<Song>, ApiError> {
+        self.get(&format!("/api/v1/setlists/{}/songs", id)).await
+    }
+
+    pub async fn get_setlist_player(&self, id: &str) -> Result<Player, ApiError> {
+        self.get(&format!("/api/v1/setlists/{}/player", id)).await
+    }
+
+    pub async fn create_setlist(&self, payload: &CreateSetlist) -> Result<Setlist, ApiError> {
+        self.post_entity("/api/v1/setlists", payload).await
+    }
+
+    pub async fn update_setlist(&self, id: &str, payload: &CreateSetlist) -> Result<Setlist, ApiError> {
+        self.put_entity(&format!("/api/v1/setlists/{}", id), payload).await
+    }
+
+    pub async fn delete_setlist(&self, id: &str) -> Result<Setlist, ApiError> {
+        self.delete_entity(&format!("/api/v1/setlists/{}", id)).await
+    }
+
+    pub async fn get_blobs(&self) -> Result<Vec<Blob>, ApiError> {
+        self.get("/api/v1/blobs").await
+    }
+
+    pub async fn get_blob(&self, id: &str) -> Result<Blob, ApiError> {
+        self.get_entity(&format!("/api/v1/blobs/{}", id)).await
+    }
+
+    pub async fn create_blob(&self, payload: &CreateBlob) -> Result<Blob, ApiError> {
+        self.post_entity("/api/v1/blobs", payload).await
+    }
+
+    pub async fn update_blob(&self, id: &str, payload: &CreateBlob) -> Result<Blob, ApiError> {
+        self.put_entity(&format!("/api/v1/blobs/{}", id), payload).await
+    }
+
+    pub async fn delete_blob(&self, id: &str) -> Result<Blob, ApiError> {
+        self.delete_entity(&format!("/api/v1/blobs/{}", id)).await
+    }
+
+    pub async fn download_blob_image(&self, id: &str) -> Result<Vec<u8>, ApiError> {
+        let response = Request::get(&Self::build_path(&format!("/api/v1/blobs/{}/image", id)))
+            .credentials(web_sys::RequestCredentials::Include)
+            .send()
+            .await?;
+
+        match response.status() {
+            200 => {
+                let bytes: Vec<u8> = response.binary().await?;
+                Ok(bytes)
+            }
+            status => Err(
+                match response.json::<ErrorResponse>().await?.to_api_error(status) {
+                    ApiError::Unauthorized(msg) => {
+                        self.route_logout();
+                        ApiError::Unauthorized(msg)
+                    }
+                    err => err,
+                },
+            ),
+        }
     }
 }
