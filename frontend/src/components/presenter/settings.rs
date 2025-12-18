2996fa4a56516203b888cdd118a57ed51ea59015
Felix Rollbühler <xilefmusics@gmail.com>
2025-12-18 16:15:50 +0100

    Improve ux for settings

--- frontend/src/components/presenter/settings.rs
+++ frontend/src/components/presenter/settings.rs
@@ -103,82 +103,108 @@ pub fn settings(props: &SettingsProps) -> Html {
 
     html! {
         <div class={Style::new(include_str!("settings.css")).expect("Unwrapping CSS should work!")}>
-            <div class="setting">
-                <label for="max-lines-per-slide">{"Max lines"}</label>
-                <input
-                    type="number"
-                    id="max-lines-per-slide"
-                    value={props.settings.max_lines_per_slide.to_string()}
-                    oninput={Callback::from(move |e: InputEvent| {
+            <div class="settings-group">
+                <div class="setting">
+                    <label for="max-lines-per-slide">
+                        <span class="material-symbols-outlined">{"format_line_spacing"}</span>
+                        {"Lines per slide"}
+                    </label>
+                    <input
+                        type="number"
+                        id="max-lines-per-slide"
+                        value={props.settings.max_lines_per_slide.to_string()}
+                        oninput={Callback::from(move |e: InputEvent| {
+                            let input: HtmlInputElement = e.target_unchecked_into();
+                            if let Ok(value) = input.value().parse::<u8>() {
+                                if value >= 1 && value <= 10 {
+                                    set_max_lines_per_slide.emit(value);
+                                }
+                            }
+                        })}
+                    />
+                </div>
+            </div>
+            <div class="settings-group">
+                <div class="setting">
+                    <label for="font-size">
+                        <span class="material-symbols-outlined">{"format_size"}</span>
+                        {"Font size"}
+                    </label>
+                    <input type="number" id="font-size" value={props.settings.font_size.to_string()} oninput={Callback::from(move |e: InputEvent| {
                         let input: HtmlInputElement = e.target_unchecked_into();
                         if let Ok(value) = input.value().parse::<u8>() {
-                            if value >= 1 && value <= 10 {
-                                set_max_lines_per_slide.emit(value);
-                            }
+                            set_font_size.emit(value);
                         }
-                    })}
-                />
+                    })}/>
+                </div>
+                <div class="setting">
+                    <label for="text-alignment">
+                        <span class="material-symbols-outlined">{"format_align_left"}</span>
+                        {"Text alignment"}
+                    </label>
+                    <select id="text-alignment" onchange={Callback::from(move |e: Event| {
+                        let select: HtmlSelectElement = e.target_unchecked_into();
+                        if let Ok(value) = select.value().parse::<TextAlignment>() {
+                            set_text_alignment.emit(value);
+                        }
+                    })}>
+                        <option value="left" selected={props.settings.text_alignment.to_select_value() == "left"}>{"Left"}</option>
+                        <option value="center" selected={props.settings.text_alignment.to_select_value() == "center"}>{"Center"}</option>
+                        <option value="right" selected={props.settings.text_alignment.to_select_value() == "right"}>{"Right"}</option>
+                    </select>
+                </div>
             </div>
-            <div class="setting">
-                <label for="background">{"Background"}</label>
-                <select id="background" onchange={Callback::from(move |e: Event| {
-                    let select: HtmlSelectElement = e.target_unchecked_into();
-                    if let Ok(value) = select.value().parse::<u8>() {
-                        set_background.emit(value);
-                    }
-                })}>
-                    <option value="0" selected={props.settings.background == 0}>{"Black"}</option>
-                    <option value="1" selected={props.settings.background == 1}>{"Red"}</option>
-                    <option value="2" selected={props.settings.background == 2}>{"Ray"}</option>
-                </select>
+            <div class="settings-group">
+                <div class="setting">
+                    <label for="text-orientation">
+                        <span class="material-symbols-outlined">{"vertical_align_center"}</span>
+                        {"Vertical position"}
+                    </label>
+                    <select id="text-orientation" onchange={Callback::from(move |e: Event| {
+                        let select: HtmlSelectElement = e.target_unchecked_into();
+                        if let Ok(value) = select.value().parse::<SlideTextOrientation>() {
+                            set_text_orientation.emit(value);
+                        }
+                    })}>
+                        <option value="top" selected={props.settings.text_orientation.to_select_value() == "top"}>{"Top"}</option>
+                        <option value="center" selected={props.settings.text_orientation.to_select_value() == "center"}>{"Center"}</option>
+                        <option value="bottom" selected={props.settings.text_orientation.to_select_value() == "bottom"}>{"Bottom"}</option>
+                    </select>
+                </div>
+                <div class="setting">
+                    <label for="horizontal-container-alignment">
+                        <span class="material-symbols-outlined">{"align_horizontal_center"}</span>
+                        {"Horizontal position"}
+                    </label>
+                    <select id="horizontal-container-alignment" onchange={Callback::from(move |e: Event| {
+                        let select: HtmlSelectElement = e.target_unchecked_into();
+                        if let Ok(value) = select.value().parse::<HorizontalContainerAlignment>() {
+                            set_horizontal_container_alignment.emit(value);
+                        }
+                    })}>
+                        <option value="left" selected={props.settings.horizontal_container_alignment.to_select_value() == "left"}>{"Left"}</option>
+                        <option value="center" selected={props.settings.horizontal_container_alignment.to_select_value() == "center"}>{"Center"}</option>
+                        <option value="right" selected={props.settings.horizontal_container_alignment.to_select_value() == "right"}>{"Right"}</option>
+                    </select>
+                </div>
             </div>
-            <div class="setting">
-                <label for="text-orientation">{"Text orientation"}</label>
-                <select id="text-orientation" onchange={Callback::from(move |e: Event| {
-                    let select: HtmlSelectElement = e.target_unchecked_into();
-                    if let Ok(value) = select.value().parse::<SlideTextOrientation>() {
-                        set_text_orientation.emit(value);
-                    }
-                })}>
-                    <option value="top" selected={props.settings.text_orientation.to_select_value() == "top"}>{"Top"}</option>
-                    <option value="center" selected={props.settings.text_orientation.to_select_value() == "center"}>{"Center"}</option>
-                    <option value="bottom" selected={props.settings.text_orientation.to_select_value() == "bottom"}>{"Bottom"}</option>
-                </select>
-            </div>
-            <div class="setting">
-                <label for="font-size">{"Font size"}</label>
-                <input type="number" id="font-size" value={props.settings.font_size.to_string()} oninput={Callback::from(move |e: InputEvent| {
-                    let input: HtmlInputElement = e.target_unchecked_into();
-                    if let Ok(value) = input.value().parse::<u8>() {
-                        set_font_size.emit(value);
-                    }
-                })}/>
-            </div>
-            <div class="setting">
-                <label for="horizontal-container-alignment">{"Horizontal container alignment"}</label>
-                <select id="horizontal-container-alignment" onchange={Callback::from(move |e: Event| {
-                    let select: HtmlSelectElement = e.target_unchecked_into();
-                    if let Ok(value) = select.value().parse::<HorizontalContainerAlignment>() {
-                        set_horizontal_container_alignment.emit(value);
-                    }
-                })}>
-                    <option value="left" selected={props.settings.horizontal_container_alignment.to_select_value() == "left"}>{"Left"}</option>
-                    <option value="center" selected={props.settings.horizontal_container_alignment.to_select_value() == "center"}>{"Center"}</option>
-                    <option value="right" selected={props.settings.horizontal_container_alignment.to_select_value() == "right"}>{"Right"}</option>
-                </select>
-            </div>
-            <div class="setting">
-                <label for="text-alignment">{"Text alignment"}</label>
-                <select id="text-alignment" onchange={Callback::from(move |e: Event| {
-                    let select: HtmlSelectElement = e.target_unchecked_into();
-                    if let Ok(value) = select.value().parse::<TextAlignment>() {
-                        set_text_alignment.emit(value);
-                    }
-                })}>
-                    <option value="left" selected={props.settings.text_alignment.to_select_value() == "left"}>{"Left"}</option>
-                    <option value="center" selected={props.settings.text_alignment.to_select_value() == "center"}>{"Center"}</option>
-                    <option value="right" selected={props.settings.text_alignment.to_select_value() == "right"}>{"Right"}</option>
-                </select>
+            <div class="settings-group">
+                <div class="setting">
+                    <label for="background">
+                        <span class="material-symbols-outlined">{"palette"}</span>
+                        {"Background"}
+                    </label>
+                    <select id="background" onchange={Callback::from(move |e: Event| {
+                        let select: HtmlSelectElement = e.target_unchecked_into();
+                        if let Ok(value) = select.value().parse::<u8>() {
+                            set_background.emit(value);
+                        }
+                    })}>
+                        <option value="0" selected={props.settings.background == 0}>{"Black"}</option>
+                        <option value="1" selected={props.settings.background == 1}>{"Red"}</option>
+                        <option value="2" selected={props.settings.background == 2}>{"Ray"}</option>
+                    </select>
+                </div>
             </div>
         </div>
     }
